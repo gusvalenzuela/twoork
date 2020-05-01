3b1c849e479b8dbbd46090455311bf835aaeb871
Gus (Nala) <gusrosales7@gmail.com>
2020-04-30 22:55:55 -0700

    changed back to public

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`Public`));
+app.use(express.static(`public`));
 
 app.use(logger("dev"));
 app.use(compression());
